392b6a61868c95da18436429293d717349e7d75a
Aquarius-blake <danbaid72@gmail.com>
2023-12-28 21:11:18 +0000

    update Home.dart

---
+++ lib/Screens/Home/Home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: PageView(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: []
+        ),
+    );
+  }
+}
