60c9ff70295d44121853989612d818a6821d8a42
Aquarius-blake <danbaid72@gmail.com>
2023-12-29 15:51:16 +0000

    update Login.dart

---
+++ lib/Screens/Authentication/Authwrapper.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Authwrapper extends StatefulWidget {
+  const Authwrapper({super.key});
+
+  @override
+  State<Authwrapper> createState() => _AuthwrapperState();
+}
+
+class _AuthwrapperState extends State<Authwrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
