f10edb68893954ddfbc51708b179fa533ce7ae75
Aquarius-blake <danbaid72@gmail.com>
2024-01-08 03:59:39 +0000

    update Splash2.dart

--- lib/Shared/Screens/Splash/Splash2.dart
+++ lib/Shared/Screens/Splash/Splash2.dart
@@ -6,37 +6,39 @@ class splash2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width*0.8,
-              height: 200,
-              child: Image.asset("assets/imgs/splash1.gif")
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width*0.8,
+                height: 200,
+                child: Image.asset("assets/imgs/splash1.gif")
+                ),
+              Text( 
+                "Best Insurance Plans Customized for You",
+                        style: const TextStyle(
+                        fontFamily: "Sofia Pro",
+                        fontSize: 34,
+                        fontWeight: FontWeight.w500,
+                        ),
+                        textAlign: TextAlign.center, ),
+              Container(),
+              ElevatedButton(
+              onPressed: (){}, 
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(154, 102, 196, 1),
+                      Color.fromRGBO(58, 75, 149, 1),
+                    ]
+                  )
+                ),
+                child: Text("Next"),
+              )
               ),
-            Text( 
-              "Best Insurance Plans Customized for You",
-                      style: const TextStyle(
-                      fontFamily: "Sofia Pro",
-                      fontSize: 34,
-                      fontWeight: FontWeight.w500,
-                      ),
-                      textAlign: TextAlign.center, ),
-            Container(),
-            ElevatedButton(
-            onPressed: (){}, 
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromRGBO(154, 102, 196, 1),
-                    Color.fromRGBO(58, 75, 149, 1),
-                  ]
-                )
-              ),
-              child: Text("Next"),
-            )
-            ),
-            ],
+              ],
+          ),
         ),
       ),
     );
